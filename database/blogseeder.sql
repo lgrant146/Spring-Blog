53cbf69f5d12c40e098539eda30153914f90095d
Leroy Grant <lgrant146@gmail.com>
2020-08-10 15:59:18 -0500

    seeder

--- database/blogseeder.sql
+++ database/blogseeder.sql
@@ -7,3 +7,6 @@ insert into posts (title, body) values
 ('Title 3', 'Body 3 asdf asdf asd fasdf as dfa sdfa sdf'),
 ('Title 4', 'Body 4 asdf asdf asd fasdf as dfa sdfa sdf'),
 ('Title 5', 'Body 5 asdf asdf asd fasdf as dfa sdfa sdf');
+
+insert into users (email, password, username) values
+(test@email, test, testuser)
